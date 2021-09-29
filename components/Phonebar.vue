95a533e2cfb1740889e0d9ca9f2302d6b48cf6a2
Shadawks <nathan.leibel67@gmail.com>
2021-09-29 17:50:01 +0200

    oui

--- components/Phonebar.vue
+++ components/Phonebar.vue
@@ -4,6 +4,18 @@
       <span>{{ item.title }}</span>
       <v-icon>{{ item.icon }}</v-icon>
     </v-btn>
+    <v-btn v-if="!this.$strapi.user" link to="/login">
+      <span>Login</span>
+      <v-icon>mdi-account-lock</v-icon>
+    </v-btn>
+    <v-btn v-if="!this.$strapi.user" link to="/register">
+      <span>Register</span>
+      <v-icon>mdi-account-plus</v-icon>
+    </v-btn>
+    <v-btn v-if="!this.$strapi.user && this.$strapi.user.role.name == 'Recruiter'" link to="/register">
+      <span>Recruitement</span>
+      <v-icon>mdi-briefcase-plus</v-icon>
+    </v-btn>
   </v-bottom-navigation>
 </template>
 
@@ -20,16 +32,6 @@
           title: 'About',
           icon: 'mdi-information',
           link: "/about"
-        },
-        {
-          title: 'Login',
-          icon: 'mdi-account-lock',
-          link: "/login"
-        },
-        {
-          title: 'Register',
-          icon: 'mdi-account-plus',
-          link: "/register"
         },
       ],
     }),
